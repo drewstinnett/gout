2b42801f76a3c2224fb41e1c7b074b6a527d191a
Drew Stinnett <drew.stinnett@duke.edu>
2021-04-10 11:09:06 -0400

    Adding more tests

--- formatter/format_test.go
+++ formatter/format_test.go
@@ -15,3 +15,25 @@ func TestInvalidOutputFormat(t *testing.T) {
 		)
 	}
 }
+
+func TestGetFormats(t *testing.T) {
+	formats := GetFormats()
+	if !stringInSlice("json", formats) {
+		t.Fatalf("GetFormats did not return json")
+	}
+	if !stringInSlice("yaml", formats) {
+		t.Fatalf("GetFormats did not return yaml")
+	}
+	if !stringInSlice("tsv", formats) {
+		t.Fatalf("GetFormats did not return tsv")
+	}
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
