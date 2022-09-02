976329ab8956d41199d4bb62f8069878ebb78144
Drew Stinnett <drew@drewlink.com>
2022-09-01 20:24:51 -0400

    feature: Multiple interface printing

--- writer_test.go
+++ writer_test.go
@@ -2,8 +2,10 @@ package writer
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 
+	"github.com/drewstinnett/go-output-format/v2/formats/json"
 	"github.com/drewstinnett/go-output-format/v2/formats/plain"
 	"github.com/stretchr/testify/require"
 )
@@ -35,4 +37,27 @@ func TestPrintError(t *testing.T) {
 
 	unprintable := make(chan int)
 	require.Panics(t, func() { c.MustPrint(unprintable) })
+	require.Panics(t, func() { c.MustPrintMulti(unprintable) })
+}
+
+func TestWriterPrinterMulti(t *testing.T) {
+	c, err := NewClient()
+	require.NoError(t, err)
+	var buf bytes.Buffer
+	c.SetWriter(&buf)
+	c.PrintMulti(
+		struct{ Foo string }{Foo: "bar"},
+		struct{ Year int }{Year: 1978},
+	)
+	require.Equal(t, "- foo: bar\n- year: 1978\n", buf.String())
+}
+
+func TestWriterAddNewlines(t *testing.T) {
+	c, err := NewClient()
+	require.NoError(t, err)
+	c.SetFormatter(json.Formatter{})
+	var buf bytes.Buffer
+	c.SetWriter(&buf)
+	c.Print(struct{ Foo string }{Foo: "bar"})
+	require.Equal(t, true, strings.HasSuffix(buf.String(), "\n"), "Printer did not have a linebreak suffix")
 }
